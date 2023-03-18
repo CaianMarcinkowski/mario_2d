a3c97fa30e820985927e8eeaa98ac08d585d28e8
Caian Marcinkowski Ferreira <caianferre@gmail.com>
2023-03-18 15:46:22 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    },500);
+}
+
+const loop = setInterval( () => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+ 
+    if(pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
+
+        pipe.style.animation = 'nome'
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'nome'
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '80px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump);
